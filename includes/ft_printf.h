ac2df569c313a658d1235ef0f6903dad55995ab0
fnacarellidev <nacarellifabio@hotmail.com>
2022-10-04 17:44:14 +0200

    add new prototype for function that prints address

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,12 +6,14 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 03:07:53 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/10/04 16:27:20 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/10/04 17:11:35 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+# define HEX_DIGITS_LOWCASE "0123456789abcdef"
+# define HEX_DIGITS_UPPERCASE "0123456789ABCDEF"
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdarg.h>
@@ -23,5 +25,6 @@ int		ft_puthex(long long x, char uppercase);
 int		ft_putstr(char *str);
 int		ft_putchar(char c);
 int		ft_percent_handler(char c, va_list ptr);
+int		ft_put_address(unsigned long int nbr);
 
 #endif
