250c23e756ee6c8fd07ebb32d46e1c2d762120c1
fnacarellidev <nacarellifabio@hotmail.com>
2022-10-04 16:33:11 +0200

    return seems to be okay for trivial cases

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,17 +6,39 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 11:22:50 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/09/28 16:11:55 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/10/04 16:32:47 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/ft_printf.h"
 #include <stdio.h>
 
-void	ft_percent_handler(char c, va_list ptr);
-
-void	ft_putchar(char c)
+int		ft_putchar(char c)
 {
-	write(1, &c, 1);
+	return (write(1, &c, 1));
+}
+
+int		ft_percent_handler(char c, va_list ptr)
+{
+	int	chars_printed;
+
+	chars_printed = 0;
+	if (c == 'c')
+		chars_printed += ft_putchar(va_arg(ptr, int));
+	else if (c == 's')
+		chars_printed += ft_putstr(va_arg(ptr, char *));
+	else if (c == 'p')
+		return (0);
+	else if (c == 'd' || c == 'i')
+		chars_printed += ft_putnbr_base(va_arg(ptr, long long), "0123456789");
+	else if (c == 'u')
+		return (0);
+	else if (c == 'x')
+		chars_printed += ft_putnbr_base(va_arg(ptr, long long), "0123456789abcdef");
+	else if (c == 'X')
+		chars_printed += ft_putnbr_base(va_arg(ptr, long long), "0123456789ABCDEF");
+	else if (c == '%')
+		return(write(1, "%", 1));
+	return (chars_printed);
 }
 
 int	ft_printf(const char *str, ...)
@@ -32,45 +54,12 @@ int	ft_printf(const char *str, ...)
 	{
 		if (str[i] == '%')
 		{
-			ft_percent_handler(str[++i], ptr);
+			count += ft_percent_handler(str[++i], ptr);
 		}
 		else
-			ft_putchar(str[i]);
+			count += ft_putchar(str[i]);
 		i++;
-		count++;
 	}
 	va_end(ptr);
 	return (count);
-}
-
-void	ft_percent_handler(char c, va_list ptr)
-{
-	if (c == 'c')
-		ft_putchar(va_arg(ptr, int));
-	else if (c == 's')
-		ft_putstr(va_arg(ptr, char *));
-	else if (c == 'p')
-		return ;
-	else if (c == 'd' || c == 'i')
-		ft_putnbr_base(va_arg(ptr, long long), "0123456789");
-	else if (c == 'u')
-		return ;
-	else if (c == 'x')
-		ft_puthex(va_arg(ptr, long long), 'x');
-	else if (c == 'X')
-		ft_puthex(va_arg(ptr, long long), 'X');
-	else if (c == '%')
-		write(1, "%", 1);
-}
-
-int main()
-{
-	int x;
-	int y;
-
-	x = ft_printf("oi %d %x %X %%%%%%\n", 10, 2031123, 589466463);
-	y = printf("oi %d %x %X %%%%%%\n", 10, 2031123,  589466463);
-	printf("X = %d\n", x);
-	printf("Y = %d\n", y);
-	return (0);
 }
